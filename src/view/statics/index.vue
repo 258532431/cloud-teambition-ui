1ce58e539f603215ad60d0d9eafe454a1b0fd6c7
yangchenglong <258532431@qq.com>
2019-08-08 17:05:38 +0800

    echarts图表

--- src/view/statics/index.vue
+++ src/view/statics/index.vue
@@ -1,12 +1,69 @@
 <template>
     <div class="main-container">
-        <div class="echarts">
-            <!-- ref 被用来给DOM元素或子组件注册引用信息,想要在Vue中直接操作DOM元素，就必须用ref属性进行注册 -->
-            <div class="chart" ref="myEchartLine" >
-            </div>
+        <div id="main" style="width:800px;height:600px;">
         </div>
     </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+export default {
+    data() {
+        return {
+            charts: '',
+        /*  opinion: ["1", "3", "3", "4", "5"],*/
+            opinionData: ["78", "88", "89", "100", "77", "60", "0"],
+            opinionData2: ["22", "12", "11", "0", "23", "40", "100"]
+        }
+    },
+    methods: {
+        drawLine(id) {
+            this.charts = echarts.init(document.getElementById(id))
+            this.charts.setOption({
+                //鼠标放到折线图上展示的数据展示样式
+                tooltip: {
+                    trigger: 'axis'  // axis   item   none三个值
+                },
+                legend: {
+                    data: ['近七日任务完成率%', '近七日任务未完成率%']
+                },
+                toolbox: {
+                    feature: {
+                        saveAsImage: {}
+                    }
+                },
+                //配置x轴数据、样式、名称
+                xAxis: {
+                    type: 'category',
+                    name: '星期',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],   // x轴数据
+                },
+                ////配置y轴样式、名称
+                yAxis: {
+                    type: 'value',
+                    name: '完成率%'
+                },
+                //y轴数据，每条折线的名称
+                series: [
+                    {
+                        name: '近七日任务完成率%',
+                        type: 'line',
+                        data: this.opinionData
+                    },
+                    {
+                        name: '近七日任务未完成率%',
+                        type: 'line',
+                        data: this.opinionData2
+                    }
+                ]
+            })
+        }
+    },
+    //调用
+    mounted() {
+        this.$nextTick(function() {
+            this.drawLine('main')
+        })
+    }
+}
 </script>
