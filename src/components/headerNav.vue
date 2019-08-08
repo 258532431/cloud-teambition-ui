28447e4b9eac7f39291963141b0374479a8f305d
yangchenglong <258532431@qq.com>
2019-08-08 17:33:42 +0800

    头部

--- src/components/headerNav.vue
+++ src/components/headerNav.vue
@@ -2,7 +2,11 @@
   <section>
     <mu-appbar style="width: 100%;" color="primary" z-depth="0">
       项目管理系统
-      <mu-button flat slot="right">登录</mu-button>
+      <mu-card-header title="张三" subTitle="技术部" slot="right">
+        <mu-avatar slot="avatar">
+        </mu-avatar>
+      </mu-card-header>
+      <mu-button slot="right" flat>退出登录</mu-button>
     </mu-appbar>
   </section>
 </template>
