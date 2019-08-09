23e2dd5a1de1d2142edaf466d3fb1b19ecfe3f6c
yangchenglong <258532431@qq.com>
2019-08-09 17:31:23 +0800

    state数据刷新页面丢失问题

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -12,11 +12,21 @@
     import headerNav from '../components/headerNav'
     import menuNav from '../components/menuNav'
     export default {
-    components:{
-      headerNav,
-      menuNav
+      components:{
+        headerNav,
+        menuNav
+      },
+      created(){
+        //在页面加载时读取sessionStorage里的状态信息, 解决刷新页面state数据丢失问题
+        if (sessionStorage.getItem("store") ) {
+          this.$store.replaceState(Object.assign({}, this.$store.state, JSON.parse(sessionStorage.getItem("store"))))
+        }
+        //在页面刷新时将vuex里的信息保存到sessionStorage里
+        window.addEventListener("beforeunload",()=>{
+          sessionStorage.setItem("store", JSON.stringify(this.$store.state))
+        })
+      }
     }
-  }
 </script>
 
 <style scoped>
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -4,16 +4,19 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+    //存储状态（变量）
     state: {
         //菜单初始选中值
         activeIndex: '1'
     },
+    //修改状态，并且是同步的，在组件中使用$store.commit('',params)
     mutations: {
         //改变菜单选中值
         changeActiveIndex(state, index) {
             state.activeIndex = index
         }
     },
+    //异步操作。在组件中使用是$store.dispath('')
     actions: {
 
     }
